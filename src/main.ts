0f700f3198cf25ca78eaddbd71388e314a8089ec
Andy Hernandez <andy.hernandezm@pca.edu.co>
2026-01-31 00:55:40 +0000

    botón de cierre de menú

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter } from '@angular/router';
+import { PreloadAllModules, RouteReuseStrategy, provideRouter, withPreloading } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
@@ -8,6 +8,20 @@ import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
 import { IonicStorageModule } from '@ionic/storage-angular';
+import { addIcons } from 'ionicons';
+import * as allIcons from 'ionicons/icons'
+
+addIcons(allIcons);
+
+bootstrapApplication (AppComponent, {
+  providers: [
+    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    provideIonicAngular(),
+    provideRouter (routes, withPreloading(PreloadAllModules)),
+    Storage
+  ],
+}
+)
 
 if (environment.production) {
   enableProdMode();
